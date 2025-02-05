731bc0513360d0c4ef07a3af597946264f34e009
Katoshi12 <bulat.bulatov.99@bk.ru>
2025-02-05 22:53:58 +0300

    Feat: Vue Router with basic routes

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/dashboard',
+    component: import('@/views/Dashboard.vue'),
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
